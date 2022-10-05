9b1f838c98e79ee992dcbd3bcb79ee498d92435d
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-10-05 14:06:43 +0100

    update

---
+++ start javascript/intro.js
@@ -0,0 +1,12 @@
+var weed = "Gloria"
+let goodStudent = "Tobi"
+const freed = "right"
+
+let number = "50"
+
+
+console.log(typeof (number))
+
+console.log(weed);
+console.log(goodStudent);
+console.log(freed);
