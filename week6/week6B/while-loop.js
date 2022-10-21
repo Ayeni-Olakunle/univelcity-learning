f4f72b09f92032fe6713b8e63aa3c4f37d1a76a3
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-10-21 15:44:24 +0100

    update

--- week6/week6B/while-loop.js
+++ week6/week6B/while-loop.js
@@ -5,7 +5,7 @@ const bankInfo = [
         bankName: "GTBank",
     },
     {
-        fullName: 'Tom Timoty',
+        fullName: 'Tom Shelby',
         amount: 500000,
         bankName: "Union Bank",
     },
@@ -21,17 +21,104 @@ const bankInfo = [
     },
 ]
 
-let count = 0;
-while (count < bankInfo.length) {
-    document.getElementById("holdBankInfo").innerHTML += `<div class="holdContent">
-    <div class="infoOne">
-        <h4 class="fullName">${bankInfo[count].fullName}</h4>
-        <p class="amount">${bankInfo[count].amount}</p>
-    </div>
-        <div class="infoTwo">
-        <h4 class="bankName">${bankInfo[count].bankName}</h4>
-        <p date="date">12-02-2022</p>
-    </div>
-</div>`
-    count++;
-}
+// let count = 0;
+// while (count < bankInfo.length) {
+//     document.getElementById("holdBankInfo").innerHTML += `<div class="holdContent">
+//     <div class="infoOne">
+//         <h4 class="fullName">${bankInfo[count].fullName}</h4>
+//         <p class="amount">${bankInfo[count].amount}</p>
+//     </div>
+//         <div class="infoTwo">
+//         <h4 class="bankName">${bankInfo[count].bankName}</h4>
+//         <p date="date">12-02-2022</p>
+//     </div>
+// </div>`
+//     count++;
+// }
+
+// JavaScript Do While Loop
+
+// let count = 0;
+
+// do {
+//     document.getElementById("holdBankInfo").innerHTML += `<div class="holdContent">
+//         <div class="infoOne">
+//             <h4 class="fullName">${bankInfo[0].fullName}</h4>
+//             <p class="amount">${bankInfo[count].amount}</p>
+//         </div>
+//             <div class="infoTwo">
+//             <h4 class="bankName">${bankInfo[count].bankName}</h4>
+//             <p date="date">12-02-2022</p>
+//         </div>
+//     </div>`
+//     count++;
+// }
+
+// while (10 < bankInfo.length)
+
+
+// for (let i = 0; i < bankInfo.length; i++) {
+//     document.getElementById("holdBankInfo").innerHTML += `<div class="holdContent">
+//         <div class="infoOne">
+//             <h4 class="fullName">${bankInfo[i].fullName}</h4>
+//             <p class="amount">${bankInfo[i].amount}</p>
+//         </div>
+//             <div class="infoTwo">
+//             <h4 class="bankName">${bankInfo[i].bankName}</h4>
+//             <p date="date">12-02-2022</p>
+//         </div>
+//     </div>`
+// }
+
+// bankInfo.forEach((item) => {
+//     document.getElementById("holdBankInfo").innerHTML += `<div class="holdContent">
+//         <div class="infoOne">
+//             <h4 class="fullName">${item.fullName}</h4>
+//             <p class="amount">${item.amount}</p>
+//         </div>
+//             <div class="infoTwo">
+//             <h4 class="bankName">${item.bankName}</h4>
+//             <p date="date">12-02-2022</p>
+//         </div>
+//     </div>`
+// });
+
+
+// bankInfo.map((item) => {
+//     document.getElementById("holdBankInfo").innerHTML += `<div class="holdContent">
+//         <div class="infoOne">
+//             <h4 class="fullName">${item.fullName}</h4>
+//             <p class="amount">${item.amount}</p>
+//         </div>
+//             <div class="infoTwo">
+//             <h4 class="bankName">${item.bankName}</h4>
+//             <p date="date">12-02-2022</p>
+//         </div>
+//     </div>`
+// });
+
+// for (let x in bankInfo) {
+//     document.getElementById("holdBankInfo").innerHTML += `<div class="holdContent">
+//         <div class="infoOne">
+//             <h4 class="fullName">${bankInfo[x].fullName}</h4>
+//             <p class="amount">${bankInfo[x].amount}</p>
+//         </div>
+//             <div class="infoTwo">
+//             <h4 class="bankName">${bankInfo[x].bankName}</h4>
+//             <p date="date">12-02-2022</p>
+//         </div>
+//     </div>`
+// }
+
+// for (let x of bankInfo) {
+//     document.getElementById("holdBankInfo").innerHTML += `<div class="holdContent">
+//         <div class="infoOne">
+//             <h4 class="fullName">${x.fullName}</h4>
+//             <p class="amount">${x.amount}</p>
+//         </div>
+//             <div class="infoTwo">
+//             <h4 class="bankName">${x.bankName}</h4>
+//             <p date="date">12-02-2022</p>
+//         </div>
+//     </div>`
+// }
