ad49d89fb57d15781ab47183e7eb4c9fbed02d2d
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-10-26 14:09:56 +0100

    update

--- week6/week6B/while-loop.js
+++ week6/week6B/while-loop.js
@@ -122,3 +122,8 @@ const bankInfo = [
 //         </div>
 //     </div>`
 // }
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+}
