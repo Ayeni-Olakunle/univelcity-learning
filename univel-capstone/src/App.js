1ac1ea05609a53540f79a19d69b17b77148e3911
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-11-04 15:48:26 +0100

    update capstone project

--- univel-capstone/src/App.js
+++ univel-capstone/src/App.js
@@ -1,13 +1,22 @@
 import appStyle from './App.module.css';
+import { Routes, Route } from 'react-router-dom';
 import "./global.css"
 import Index from './components/information/index';
 import Navigate from './components/navigation/navigate';
+import LandingPage from './components/content/landingPage';
+import Service from './components/content/service';
 
 function App() {
   return (
     <div className={appStyle.app}>
       <div className={appStyle.info}>
         <Index />
+      </div>
+      <div className={appStyle.content}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/services" element={<Service />} />
+        </Routes>
       </div>
       <div className={appStyle.navigate}>
         <Navigate />
